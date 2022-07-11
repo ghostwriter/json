ddeb0d60d203458726a3594cbe74d042e25d2ef8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-11 17:10:15 -0500

    Update Json.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Json.php
+++ src/Json.php
@@ -10,26 +10,36 @@ use UnexpectedValueException;
 
 final class Json implements JsonInterface
 {
+    /** @var int */
+    public const DECODE = JSON_BIGINT_AS_STRING | JSON_OBJECT_AS_ARRAY | JSON_THROW_ON_ERROR;
+
+    /** @var int */
+    public const DEPTH = 512;
+
+    /** @var int */
+    public const EMPTY = 0;
+
+    /** @var int */
+    public const ENCODE = JSON_PRESERVE_ZERO_FRACTION | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_THROW_ON_ERROR;
+
+    /** @var int */
+    public const PRETTY = self::ENCODE | JSON_PRETTY_PRINT;
+
     public static function decode(string $json): mixed
     {
         try {
-            return json_decode($json, true, 512, self::DECODE);
+            return json_decode($json, true, self::DEPTH, self::DECODE);
         } catch (JsonException $e) {
             throw new UnexpectedValueException($e->getMessage());
         }
     }
 
-    public static function encode(mixed $data, ?int $options = null): string
+    public static function encode(mixed $data, int $flags = self::EMPTY): string
     {
         try {
-            return json_encode($data, ($options ?? 0) | self::ENCODE);
+            return json_encode($data, $flags | self::ENCODE, self::DEPTH);
         } catch (JsonException $e) {
             throw new UnexpectedValueException($e->getMessage());
         }
-    }
-
-    public static function prettyPrint(mixed $data, ?int $options = null): string
-    {
-        return self::encode($data, ($options ?? 0) | self::PRETTY);
     }
 }
