ccb62dc95e809475224463e40d07e4b820a277d9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-09 13:20:57 -0600

    Update JsonException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/JsonException.php
+++ src/Exception/JsonException.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Json\Exception;
 
+use Ghostwriter\JsonTests\Unit\Exception\JsonExceptionTest;
 use Ghostwriter\Json\Interface\JsonExceptionInterface;
 use RuntimeException;
 
 /**
- * @see \Ghostwriter\JsonTests\Unit\Exception\JsonExceptionTest
+ * @see JsonExceptionTest
  */
 final class JsonException extends RuntimeException implements JsonExceptionInterface
 {
